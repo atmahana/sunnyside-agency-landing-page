f6c258a51b77f94cb8a2826e762b46fc9b3f418f
Adham <zubairadham.ar@gmail.com>
2023-06-10 23:06:44 +0800

    --done mobile layout --wip mobile navbar

---
+++ src/navbar.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName("toggle-button");
+
+const navbarLinks = document.getElementsByClassName("navbar__links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
