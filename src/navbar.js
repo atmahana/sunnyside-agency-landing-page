5aab0d93959bf2d515b99de4949e1a0c5e652f8b
Adham <zubairadham.ar@gmail.com>
2023-06-12 21:59:28 +0800

    feat: added responsive navigation bar

--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,9 @@
-const toggleButton = document.getElementsByClassName("toggle-button");
+const toggleButton = document.querySelector(".toggle-button");
 
-const navbarLinks = document.getElementsByClassName("navbar__links")[0];
+const navbarLinks = document.getElementsByClassName("navbar__links");
+
+console.log(toggleButton)
 
 toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
+  navbarLinks[0].classList.toggle("active");
 });
